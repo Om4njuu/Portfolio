31a89b7a270093e6b4954948f5d56407d467159a
om4njuu <omanjuu.20@gmail.com>
2024-12-30 19:47:04 -1000

    Updates

--- script.js
+++ script.js
@@ -33,22 +33,6 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 
-/* ========== Swiper ========== */
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: true,
-    grabCursor: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
-
 /* ========== Dark Mode Toggle ========== */
 let darkModeIcon = document.querySelector('#darkMode-icon');
 
